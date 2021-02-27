83245cc832f8602ec3ed3c12de640582439453ca
adrian.fuchs <adrian@corp.kionux.com.br>
2021-02-27 13:02:12 -0300

    separando styles, adicionando funcionalidades aos botoes

--- js/script.js
+++ js/script.js
@@ -10,4 +10,80 @@ $(document).ready(() => {
 		i ++
 		i %= colors.length
 	})
+
+	$("#paleta_de_cores").click(function() {
+		console.log("#paleta_de_cores")
+		// $(this).find('path').css({
+		// 	fill: colors[i]
+		// })
+	});
+
+	let remover_estados = false;
+	$("#remover_estados").click(function() {
+		console.log("#remover_estados")
+		remover_estados = !remover_estados;
+		if(remover_estados){
+			$("#remover_estados").css({
+				background : "red"
+			})
+		}else{
+			$("#remover_estados").css({
+				background : "transparent"
+			})
+		}
+		$('body').on('click', 'a', function() {
+			if($(this).hasClass("removido")){
+				$(this).removeClass("removido")
+				$(this).find('path').css({
+					fill: "#dbcc72"
+				})
+			}else{
+				$(this).addClass("removido")
+				$(this).find('path').css({
+					fill: "#A9A9A9"
+				})
+			}
+		})		
+	});
+	
+	$("#selecionar_estado").click(function() {
+		console.log("#selecionar_estado")
+	});
+	
+	$("#algoritmo_a").click(function() {
+		console.log("#algoritmo_a")
+	});
+	
+	$("#algoritmo_b").click(function() {
+		console.log("#algoritmo_b")
+	});
+	
+	$("#algoritmo_c").click(function() {
+		console.log("#algoritmo_c")
+	});
+	
+	let algoritmo_a = false;
+	let first_time_algoritmo_a = true;
+	$("#algoritmo_a").click(function() {
+		console.log("#algoritmo_a")
+		algoritmo_a = !algoritmo_a;
+		if(first_time_algoritmo_a){
+			first_time_algoritmo_a = false;
+			$('#textarea').removeClass("display-none");
+		}
+		if(algoritmo_a){
+			$('#textarea').removeClass("m-fadeOut");
+			$('#textarea').addClass("m-fadeIn");
+			$('#buttons-div').addClass("display-none");
+		}else{
+			$('#textarea').removeClass("m-fadeIn");
+			$('#textarea').addClass("m-fadeOut");
+			$('#buttons-div').removeClass("display-none");
+		}
+	});
+	
+	$("#direitos_autorais").click(function() {
+		console.log("#direitos_autorais")
+	});
+
 })
