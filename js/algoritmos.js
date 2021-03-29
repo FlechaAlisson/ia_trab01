5957619e69776bcd999e6874293875425be6052c
Alisson Flecha <alisson@corp.kionux.com.br>
2021-03-29 09:31:03 -0300

    srcblock com dados corretos

--- js/algoritmos.js
+++ js/algoritmos.js
@@ -1,6 +1,6 @@
 
 import border_list from './estados.js'
-import {defColor} from "./colors.js"
+import { defColor } from "./colors.js"
 let int_num = 0
 
 
@@ -38,11 +38,16 @@ function* largura(nome, cores, lista_removido, textarea, src) {
 	for (let i = 0; i < lista_not_pintados.length; i++) {
 		const cor = getPossibleColors(lista_not_pintados[i], cores)
 		setColor(lista_not_pintados[i], cor[0])
+
+		textarea.val(src)
+		textarea.val(textarea.val() + "\nverificando:" + lista_not_pintados[i]);
+		textarea.val(textarea.val() + "\ncores possíveis:" + cor);
+		textarea.val(textarea.val() + "\ncor escolhida:" + cor[0]);
 		yield lista_not_pintados[i]
 		buscaEstado(lista_not_pintados[i]).neighbors.forEach(element => {
 			if (buscaEstado(element).color == undefined &&
-			 !lista_not_pintados.includes(element) &&
-			 !lista_removido.includes(element)) {
+				!lista_not_pintados.includes(element) &&
+				!lista_removido.includes(element)) {
 				lista_not_pintados.push(element)
 			}
 		});
@@ -51,28 +56,28 @@ function* largura(nome, cores, lista_removido, textarea, src) {
 }
 
 
-function * algoritmoC(nome, cores, lista_removido, textarea, src) {
-	textarea.val( textarea.val() + "\nverificando:" + nome );
-	textarea.val( textarea.val() + "\n iteracao:" + int_num );
+function* algoritmoC(nome, cores, lista_removido, textarea, src) {
+	textarea.val(textarea.val() + "\nverificando:" + nome);
+	textarea.val(textarea.val() + "\n iteracao:" + int_num);
 	int_num++
 	console.log(int_num);
 	//Busca a instância do Estado
 	const state = buscaEstado(nome)
-	textarea.val( textarea.val() + "\n achou:" + state.name );
+	textarea.val(textarea.val() + "\n achou:" + state.name);
 	//Busca as cores possíveis
 	const cor = getPossibleColors(nome, cores)
-	textarea.val( textarea.val() + "\n cores possiveis:" + cor );
+	textarea.val(textarea.val() + "\n cores possiveis:" + cor);
 	//seta a cor
 	setColor(nome, cor[0])
-	textarea.val( textarea.val() + "\n pintando com:" + cor[0] );
+	textarea.val(textarea.val() + "\n pintando com:" + cor[0]);
 	//Para todo vizinho, chama recursivamente
 
 	yield nome
 
 	for (let neighbor of state.neighbors) {
 
-		textarea.val( src );
-		if(lista_removido.includes(nome)){
+		textarea.val(src);
+		if (lista_removido.includes(nome)) {
 			// console.log(nome + " ignorado")
 			continue;
 		}
@@ -84,22 +89,28 @@ function * algoritmoC(nome, cores, lista_removido, textarea, src) {
 	}
 }
 
-function * BuscaMenorConflito(name, cores, lista_removido, textarea, src) {
+function* BuscaMenorConflito(name, cores, lista_removido, textarea, src) {
+	textarea.val(textarea.val() + "\nverificando:" + name);
+	textarea.val(textarea.val() + "\n iteracao:" + int_num);
 	console.log(name);
 	int_num++
 	console.log(int_num);
-	yield name
 	let state = buscaEstado(name)
+	textarea.val(textarea.val() + "\n achou:" + state.name);
 	let coresPossiveis = getPossibleColors(name, cores)
+	textarea.val(textarea.val() + "\n cores possiveis:" + coresPossiveis);
 	setColor(state.name, coresPossiveis[0])
-	let neighbors = state.neighbors.sort((a,b) => {
+
+	textarea.val(textarea.val() + "\n pintando com:" + coresPossiveis[0]);
+	let neighbors = state.neighbors.sort((a, b) => {
 		return buscaEstado(a).neighbors.length - buscaEstado(b).neighbors.length
 	})
-	
+	yield name
 	console.log(neighbors);
 	for (const neighbor of neighbors) {
 		var color = buscaEstado(neighbor).color
-		if (!color || color == defColor){
+		if (!color || color == defColor) {
+			textarea.val(src);
 			yield* BuscaMenorConflito(neighbor, cores, lista_removido, textarea, src)
 		}
 	}
